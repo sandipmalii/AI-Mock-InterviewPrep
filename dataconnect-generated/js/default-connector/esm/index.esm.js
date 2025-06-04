ccb4ef68e6b799f4776e6427573cfd8f7635575c
sandipmalii <21121a0567@vidyanikethan.asia>
2025-06-04 22:52:01 +0530

    Initial Commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'monk-interview',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'monk-interview',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
